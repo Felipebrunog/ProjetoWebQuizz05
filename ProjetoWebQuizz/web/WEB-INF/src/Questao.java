f7ad30c6b0d661ea33e33282661e60cc28610822
Gustavo <Gustavo@Gustavo-HP>
2017-05-16 21:43:03 -0300

    Implementação classe Questao

---
+++ ProjetoWebQuizz/web/WEB-INF/src/Questao.java
@@ -0,0 +1,36 @@
+package WEB
+
+-INF.src;
+
+public class Questao {
+    private String questao;
+    private String resposta;
+    private String[] alternativa;
+
+    public Questao(String questao, String resposta, String[] alternativa) {
+        this.questao = questao;
+        this.resposta = resposta;
+        this.alternativa = alternativa;
+    }
+
+    public String getQuestao() {
+        return questao;
+    }
+    public void setQuestao(String questao) {
+        this.questao = questao;
+    }
+
+    public String getResposta() {
+        return resposta;
+    }
+    public void setResposta(String resposta) {
+        this.resposta = resposta;
+    }
+
+    public String[] getAlternativa() {
+        return alternativa;
+    }
+    public void setAlternativa(String[] alternativa) {
+        this.alternativa = alternativa;
+    }
+}
